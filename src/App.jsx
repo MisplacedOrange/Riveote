c569a7a8a0d9daf25ff1813394e15233f3bb4b11
MisplacedOrange <Roylu0404@gmail.com>
2025-07-30 23:42:00 -0400

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Layout = () => (
+<div className="layout">
+  <header>
+    <navbar>
+      <h1>Riveo</h1>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </navbar>
+  </header>
+  <main>
+    <h2>Welcome to Riveo</h2>
+  </main>
+  <footer>
+    <p>© 2025 Riveo. All rights reserved.</p>
+  </footer>
 
-export default App
+</div>
+);
